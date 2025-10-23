e3f8860537da5496e0f342f1675cbb00b3965298
keerthi vardhan <keerthivardhan2k@gmail.com>
2025-10-23 11:25:51 +0530

    Implement companionsList component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import CompanionCard from '@/components/CompanionCard'
 import CompanionsList from '@/components/CompanionsList'
 import CTA from '@/components/CTA'
-import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 
 const Page = () => {
   return (
-    <div>
+    <main>
       <h1 className='text-2xl underline'>Popular Companions</h1>
       <section className='home-section'>
         <CompanionCard 
@@ -36,10 +36,14 @@ const Page = () => {
       </section>
 
       <section className='home-section'>
-        <CompanionsList/>
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
         <CTA/>
       </section>
-    </div>
+    </main>
   )
 }
 
